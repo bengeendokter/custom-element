dd27c550f0cd4a141c00d540cba3c0edebe26533
bengeendokter <benifit.arts@gmail.com>
2024-02-08 21:49:09 +0100

    Add Rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,12 @@
+// rollup.config.mjs
+import typescript from '@rollup/plugin-typescript';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+export default {
+	input: 'src/custom-element.ts',
+	output: {
+		file: 'lib/custom-element.js',
+		format: 'esm'
+	},
+	plugins: [typescript(), nodeResolve({extensions: ['.ts']})]
+};
